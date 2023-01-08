a99c9ce559078fbf716c4312806fa5d97ed24f2f
Andrew Chernoff <whitedrew538@gmail.com>
2023-01-08 20:54:13 +0500

    changed quotes

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -32,7 +32,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.PRE_JUNIOR}
                         /* onClick={handleClose} */
                         // className={...} // делает студент
-                        className={({isActive}) => isActive ? s.active: ""}
+                        className={({isActive}) => isActive ?  `${s.active}` : ''}
                     >
                         Pre-junior
                     </NavLink>
@@ -41,7 +41,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR}
                         /* onClick={handleClose} */
                         // className={...} // делает студент
-                        className={({isActive}) => isActive ? s.active: ""}
+                        className={({isActive}) => isActive ?  `${s.active}` : ''}
                     >
                         Junior
                     </NavLink>
@@ -50,7 +50,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR_PLUS}
                         /* onClick={handleClose} */
                         // className={...} // делает студент
-                        className={({isActive}) => isActive ? s.active: ""}
+                        className={({isActive}) => isActive ?  `${s.active}` : ''}
                     >
                         Junior Plus
                     </NavLink>

a6068e0d0d9d663eabae40f25c58f9dc93c842b4
Andrew Chernoff <whitedrew538@gmail.com>
2023-01-28 23:26:01 +0500

    reverse method to reducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,21 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
             if (action.payload === 'up') {
                 return [...state.sort((a, b) => a.name.localeCompare(b.name))]
-
             } else {
                 return [...state.sort((a, b) => b.name.localeCompare(a.name))]
-
+                }
             }
-            }
-            
         case 'check': {
-
-            return [...state.filter(el => el.age > action.payload)]
+            return [...state.filter(el => el.age > action.payload)].reverse()
         }
         default:
             return state

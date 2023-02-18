93b08c03fb1b57b60f01dc64f04f00f82e81b865
Andrew Chernoff <whitedrew538@gmail.com>
2023-02-19 00:32:56 +0500

    slider range mui

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -3,6 +3,7 @@ import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
+import Slider from '@mui/material/Slider'
 
 /*
 * 1 - передать значения в оба слайдера
@@ -13,11 +14,18 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value2, setValue2] = useState(restoreState<number[]>('hw11-value2', [0, 100]))
 
-    const change = (event: any, value: any) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-    }
+    const handleChange = (event: Event, newValue: number | number[]) => {        
+        if (Array.isArray(newValue)) {
+            setValue1(newValue[0])
+            setValue2(newValue as number[])
+          } else {
+            setValue1(newValue)
+            let newValue2 = value2.map((el,i) => i === 0 ? newValue: el) ///new value for 'value2'
+            setValue2(newValue2)
+          }
+      };
 
     return (
         <div id={'hw11'}>
@@ -29,18 +37,27 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            getAriaLabel={() => 'Temperature range'}
+                            value={value1}
+                            onChange={handleChange}
+                            valueLabelDisplay="auto"
+                            //getAriaValueText={valuetext}
                             // сделать так чтоб value1 изменялось // пишет студент
 
                         />
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
+                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                        getAriaLabel={() => 'Temperature range'}
+                        value={value2}
+                        onChange={handleChange}
+                        valueLabelDisplay="auto"
                         />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+
+                        <span id={'hw11-value-2'} className={s.number}>{value2[1]}</span>
                     </div>
                 </div>
             </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
-const SuperRange: React.FC<SliderProps> = (props) => {
+/* type PropsType = {
+    change: ()
+} */
+
+const SuperRange: React.FC<SliderProps & any> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                width: '165px',
+                height: '4px',
+                color: '#00CC22'
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
